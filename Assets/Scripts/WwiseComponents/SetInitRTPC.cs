b2084719def1713647b810fcf201fb7fed69c78d
FranciscoFoco <fcocerda@gmail.com>
2016-07-16 13:54:23 -0400

    Wwise script for reset RTPC at startup

---
+++ Assets/Scripts/WwiseComponents/SetInitRTPC.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetInitRTPC : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+  
+    AkSoundEngine.SetRTPCValue("Intensity", 0);
+    AkSoundEngine.SetRTPCValue("Player_Kills_Progress", 0);
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
